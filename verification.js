4d5ae0a0dc29e11a7baf7b8527a40e872ee9d735
meglynst <meglynst@umich.edu>
2021-03-18 16:57:04 -0500

    Update verification.js
    add comments

--- verification.js
+++ verification.js
@@ -1,23 +1,23 @@
-const codes = require('./codeStore')
+const codes = require('./codeStore') // import our code storage map from codeStore.js
 
-const EXPIRY_INTERVAL_MS = 60 * 1000
+const EXPIRY_INTERVAL_MS = 60 * 1000 // create a default amount of time for 2FA code expiry - in this case 60000 ms or 1 min
 
 const createCode = () => {
-    return Math.floor(100000 + Math.random() * 900000).toString()
+    return Math.floor(100000 + Math.random() * 900000).toString() // create verification code as random 6 digit number represented with a string
 }
 
 const addCode = (number, code) => {
-    codes.set(number, { code: code, expiresAt: Date.now() + EXPIRY_INTERVAL_MS })
+    codes.set(number, { code: code, expiresAt: Date.now() + EXPIRY_INTERVAL_MS }) // set code with expiration time, keyed to phone number
 }
 
 const deleteCode = number => {
-    codes.delete(number)
+    codes.delete(number) // remove code
 }
 
 const verifyCode = (number, code) => {
-    if (codes.get(number).code !== code) {
+    if (codes.get(number).code !== code) { // throw err if code not present in codes map
         throw new Error('Your code does not match.')
-    } else if (Date.now() >= codes.get(number).expiresAt) {
+    } else if (Date.now() >= codes.get(number).expiresAt) { // throw err if current time is past expiry time of code
         throw new Error('Your code has expired.')
     }
 }
