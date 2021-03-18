5e4d65bfac9fe7d6b07a1309cfddf5770b6757ce
meglynst <meglynst@umich.edu>
2021-03-18 16:57:22 -0500

    Update messenger.js
    add comments

--- messenger.js
+++ messenger.js
@@ -2,9 +2,9 @@ const freeClimbSDK = require('@freeclimb/sdk')
 
 const accountId = process.env.ACCOUNT_ID
 const authToken = process.env.AUTH_TOKEN
-const freeClimb = freeClimbSDK(accountId, authToken)
+const freeClimb = freeClimbSDK(accountId, authToken) // create FreeClimb client using your account Id and auth token
 const fcNumber = process.env.FC_PHONE_NUMBER
 
 exports.sendCode = async (to, code) => {
-    await freeClimb.api.messages.create(fcNumber, to, `Your verification code is: ${code}`)
+    await freeClimb.api.messages.create(fcNumber, to, `Your verification code is: ${code}`) // send SMS message using FreeClimb client
 }
